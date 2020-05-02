f8cd0119c47ae3366d96c27dc1a3595c6f418688
darkwizard242 <am900820@gmail.com>
2020-05-02 16:29:23 -0400

    testinfra test function name correction

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,25 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_googlechrome_package_installed(host):
+def test_virtualbox_package_installed(host):
     assert host.package("virtualbox-6.1").is_installed
 
 
-def test_googlechrome_binary_exists(host):
+def test_virtualbox_binary_exists(host):
     assert host.file('/usr/bin/virtualbox').exists
 
 
-def test_googlechrome_binary_symlink(host):
+def test_virtualbox_binary_symlink(host):
     assert host.file('/usr/bin/virtualbox').is_symlink
 
 
-def test_googlechrome_binary_which(host):
+def test_virtualbox_binary_which(host):
     assert host.check_output('which virtualbox') == '/usr/bin/virtualbox'
 
 
-def test_googlechrome_repo_exists(host):
+def test_virtualbox_repo_exists(host):
     assert host.file('/etc/apt/sources.list.d/virtualbox.list').exists
 
 
-def test_googlechrome_repo_file(host):
+def test_virtualbox_repo_file(host):
     assert host.file('/etc/apt/sources.list.d/virtualbox.list').is_file
